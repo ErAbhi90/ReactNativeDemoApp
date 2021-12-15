8056fe329972d036afb73cbfdb57bae9159203c2
Wasiun Khan <wasiun@gmail.com>
2021-12-14 21:36:09 -0500

    i18next-init

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -9,18 +9,23 @@ import {
   Title,
 } from "../components/account.styles";
 
+import i18n from "../../../services/translation/i18n";
+import { useTranslation } from 'react-i18next';
+import { TouchableOpacity,StyleSheet, Text, View } from "react-native";
+
 export const AccountScreen = ({ navigation }) => {
+  const { t, i18n } = useTranslation();
   return (
     <AccountBackground>
       <AccountCover />
-      <Title>Demo App</Title>
+      <Title>{t('AppName')}</Title>
       <AccountContainer>
         <AuthButton
           icon="lock-open-outline"
           mode="contained"
           onPress={() => navigation.navigate("Login")}
         >
-          Login
+          {t('Login') }
         </AuthButton>
         <Spacer size="large">
           <AuthButton
@@ -28,10 +33,35 @@ export const AccountScreen = ({ navigation }) => {
             mode="contained"
             onPress={() => navigation.navigate("Register")}
           >
-            Register
+            {t('Register') }
           </AuthButton>
         </Spacer>
       </AccountContainer>
+      <View style={{
+        flexDirection: 'row',
+        alignContent: 'bottom',
+          margin: 10,
+        }}>
+      <TouchableOpacity
+          onPress={() => i18n.changeLanguage('en')} //Here I change the language to "en" English
+          style={Styles.button}>
+          <Text style={{color: '#fff'}}>EN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => i18n.changeLanguage('fr')} //Here I change the language to "es" Spanish
+          style={Styles.button}>
+          <Text style={{color: '#fff'}}>Fr</Text>
+        </TouchableOpacity>
+        </View>
     </AccountBackground>
   );
 };
+
+const Styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#61e3a5',
+    padding: 10,
+    borderRadius: 100,
+    margin: 10,
+  },
+});
